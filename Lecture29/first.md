016778292283b2c1b42c084cd4c390fc2efd10e5
Rohit ji <negiji@9gmail.com>
2026-02-05 22:45:43 +0530

    helo

---
+++ Lecture29/first.md
@@ -0,0 +1,6 @@
+<!-- Print all the address -->
+<!-- array print -->
+<!-- pointer as array arithmetic pointer-->
+<!-- (void*)p -->
+<!-- function & pointer , &-->
+<!-- pass by reference and pass by pointer, int a[] -->
---
+++ Lecture29/start.cpp
@@ -0,0 +1,74 @@
+#include<iostream>
+using namespace std;
+
+// int add(int *p, int *size){
+   
+//     int ans = 0;
+//     for(int i=0;i<*size;i++){
+//         ans+=p[i];
+//     }
+
+//     return ans;
+// }
+
+int main(){
+
+    // int a = 10;
+    // int *ptr = &a;
+    // // ptr is a pointer which points to integer type value
+
+    // cout<<a<<endl;
+    // // value of a
+    // cout<<&a<<endl;
+    // // address of a
+    // cout<<ptr<<endl;
+    //  // address of a
+    // cout<<&ptr<<endl;
+    //  // address of ptr
+
+
+
+    // How array works
+
+
+    // int arr[5] = {1,3,4,2,10};
+
+    // for(int i=0;i<5;i++){
+    //     cout<<arr+i<<endl;
+    // }
+
+
+    // // print the value
+
+    // for(int i=0;i<5;i++){
+    //     cout<<*(arr+i)<<endl;
+    // }
+
+    // int *p = arr;
+
+    // for(int i=0;i<5;i++){
+    //     cout<<p[i]<<endl;
+    // }
+
+    // // print address
+
+    // for(int i=0;i<5;i++){
+    //     cout<<p+i<<endl;
+    // }
+    
+
+
+    // int arr[10] = {1,2,3,4,5,6,7,8,9,10};
+    // int n = 10;
+    // int ans = add(arr,&n);
+    // cout<<ans;
+
+
+    // char arr[5] = {'a','b','c','d','e'};
+    // char *ptr = arr;
+    // cout<<(void*)ptr;
+
+    
+
+    return 0;
+}
