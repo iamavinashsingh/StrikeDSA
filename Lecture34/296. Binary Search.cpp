5bd014f015ee67334ea10ab4a99a8db6dcf38862
Rohit ji <negiji@9gmail.com>
2026-02-17 22:15:09 +0530

    hello

---
+++ Lecture34/296. Binary Search.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+
+    int binarySearch(vector<int>& nums,int start, int end, int target){
+         
+		//  base condition
+		if(start>end){
+			return -1;
+		}
+
+
+		int mid = start+(end-start)/2;
+
+		if(nums[mid]==target){
+			return mid;
+		}
+		else if(nums[mid]>target){
+			return binarySearch(nums,start,mid-1,target);
+		}
+		else{
+			return binarySearch(nums,mid+1,end,target);
+		}
+	}
+  
+
+    int search(vector<int>& nums, int target) {
+        // Write your code here
+
+		return binarySearch(nums,0,nums.size()-1,target);
+    }
+};
